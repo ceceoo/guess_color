d8559c636457f14d42b1ccabd55829597c571427
ce <xinyizhangce@outlook.com>
2020-06-23 15:52:36 +0800

    modify logics

--- guess.py
+++ guess.py
@@ -1,4 +1,5 @@
 import random
+
 def guess_color(real, guess):
     if isinstance(guess, list) and len(guess) == len(real):
         return valid_guess(real, guess)
@@ -10,17 +11,25 @@ def guess_color(real, guess):
 def valid_guess(real, guess):
     half = 0
     all = 0
-    for i in range(0, len(guess)):
+    shadow = []
+    for i in range(len(real)):
+        shadow.append(0)
+    for i in range(0, len(real)):
         if guess[i] == real[i]:
             all += 1
-        if guess[i] in real and guess[i] != real[i]:
-            half += 1
+            shadow[i] = 1
+            print(shadow)
+        elif guess[i] in real and guess[i] != real[i]:
+            j = real.index(guess[i])
+            if shadow[j] != 1:
+                half += 1
     return all, half
 
 
 if __name__ == '__main__':
     real_set = ['red', 'yellow', 'blue', 'green', 'white', 'pink']
     real = random.sample(real_set, 4)
+    print(real)
     for i in range(0, 7):
         guess_list = list(map(str, input("\nPlease input 4 colors in order, separated by spaces").strip().split()))
         all, half = guess_color(real, guess_list)
